1ee2b7e54b5667368525a53390eb966b5f8ba402
Sergey N Chursanov <masakra@mail.ru>
2012-10-31 17:33:32 +0400

    QApplication added

--- cg/src/main.cpp
+++ cg/src/main.cpp
@@ -1,16 +1,19 @@
 #include "sameVolume.h"
 
+#include <QApplication>
 #include <QMessageBox>
 #include <QDebug>
 
 int
 main( int argc, char ** argv )
 {
+	QApplication app( argc, argv );
+
 	if ( sameVolume( "C:/CONFIG.SYS", "C:/tmp/an24.csv" ) )
 		QMessageBox::information( 0, "", "Yes" );
 	else
 		QMessageBox::information( 0, "", "No" );
 
-
 	return 0;
+	//return app.exec();
 }
